bcefd3eef13289c6847d43a00b4ad866137fd48b
YOHESWARI RAJENDRAN <yohirajendran@gmail.com>
2024-12-22 21:32:02 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,17 @@
+function appendvalue(value){
+    const display=document.getElementById('display');
+    display.value +=value;
+}
+function clearDisplay(){
+    const display=document.getElementById('display');
+    display.value='';
+
+}
+function calculateResult(){
+    const display=document.getElementById('display');
+    try{
+        display.value=eval(display.value);
+        }catch{
+            display.value='Error';
+        }
+}
